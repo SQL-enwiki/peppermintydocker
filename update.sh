797adadc3811ffebc66ad652f0a8d2c87204d612
SQL-enwiki <45909509+SQL-enwiki@users.noreply.github.com>
2019-01-02 15:08:21 -0500

    Update to pull the lastest release
    Plus generate a master tag

--- update.sh
+++ update.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-cd /home/sql/myimages/webroot
-git pull
+
 cd /home/sql/myimages
-docker build /home/sql/myimages
-docker push sqlatenwiki/peppermintywiki
+rm -rf /home/sql/myimages/webroot
+echo "###CLONING LATEST RELEASE###"
+git clone https://github.com/sbrl/Pepperminty-Wiki.git webroot
+cd /home/sql/myimages/webroot
+LATEST="$(git describe --abbrev=0 --tags)"
+echo "###CHECKING OUT RELEASE $LATEST###"
+git checkout $LATEST
+cd /home/sql/myimages
+echo "###BUILDING RELEASE $LATEST###"
+docker build -t sqlatenwiki/peppermintywiki:$LATEST /home/sql/myimages
+echo "###PUSHING RELEASE $LATEST###"
+docker push sqlatenwiki/peppermintywiki:$LATEST
+
+
+cd /home/sql/myimages                                                                                                                                                                             
+rm -rf /home/sql/myimages/webroot                                                                                                                                                                 
+echo "###CLONING MASTER###"                                                                                                                                                                              
+git clone https://github.com/sbrl/Pepperminty-Wiki.git webroot                                                                                                                                    
+cd /home/sql/myimages/webroot                                                                                                                                                                     
+echo "###CHECKING OUT MASTER###"
+cd /home/sql/myimages
+echo "###BUILDING MASTER###"
+docker build -t sqlatenwiki/peppermintywiki:master /home/sql/myimages
+echo "###PUSHING master###"
+docker push sqlatenwiki/peppermintywiki:master
