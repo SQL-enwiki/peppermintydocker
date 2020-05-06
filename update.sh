3e784bdc5f50a4579d2a1e752608732721082b99
SQL At ENWiki <45909509+SQL-enwiki@users.noreply.github.com>
2020-05-06 19:49:35 -0400

    may as well support every arch, no?

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ARCHS="linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386"
 cd /home/sql/myimages
 rm -rf /home/sql/myimages/webroot
 echo "###CLONING###"
@@ -10,7 +10,7 @@ echo "###CHECKING OUT $LATEST###"
 git checkout $LATEST
 cd /home/sql/myimages
 echo "###BUILDING AND PUSHING $LATEST###"
-docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -t sqlatenwiki/peppermintywiki:latest -t sqlatenwiki/peppermintywiki:stable -t sqlatenwiki/peppermintywiki:$LATEST  . --push
+docker buildx build --platform $ARCHS -t sqlatenwiki/peppermintywiki:latest -t sqlatenwiki/peppermintywiki:stable -t sqlatenwiki/peppermintywiki:$LATEST  . --push
 
 cd /home/sql/myimages
 rm -rf /home/sql/myimages/webroot
@@ -19,4 +19,4 @@ git clone https://github.com/sbrl/Pepperminty-Wiki.git webroot
 cd /home/sql/myimages/webroot
 chown www-data:www-data * -R
 cd /home/sql/myimages
-docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -t sqlatenwiki/peppermintywiki:dev -t sqlatenwiki/peppermintywiki:master . --push
+docker buildx build --platform $ARCHS -t sqlatenwiki/peppermintywiki:dev -t sqlatenwiki/peppermintywiki:master . --push
