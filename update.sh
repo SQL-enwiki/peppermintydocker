ff4d0524cb09467efe122e033c54c84be84d772e
SQL At ENWiki <45909509+SQL-enwiki@users.noreply.github.com>
2020-05-06 19:34:24 -0400

    update for buildx

--- update.sh
+++ update.sh
@@ -9,13 +9,8 @@ LATEST="$(git describe --abbrev=0 --tags)"
 echo "###CHECKING OUT $LATEST###"
 git checkout $LATEST
 cd /home/sql/myimages
-echo "###BUILDING $LATEST###"
-docker build -t sqlatenwiki/peppermintywiki:$LATEST -t sqlatenwiki/peppermintywiki:stable /home/sql/myimages
-echo "###PUSHING $LATEST###"
-docker push sqlatenwiki/peppermintywiki:$LATEST
-docker push sqlatenwiki/peppermintywiki:stable
-docker push sqlatenwiki/peppermintywiki:latest
-
+echo "###BUILDING AND PUSHING $LATEST###"
+docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -t sqlatenwiki/peppermintywiki:latest -t sqlatenwiki/peppermintywiki:stable -t sqlatenwiki/peppermintywiki:$LATEST  . --push
 
 cd /home/sql/myimages
 rm -rf /home/sql/myimages/webroot
@@ -24,8 +19,4 @@ git clone https://github.com/sbrl/Pepperminty-Wiki.git webroot
 cd /home/sql/myimages/webroot
 chown www-data:www-data * -R
 cd /home/sql/myimages
-echo "###BUILDING MASTER###"
-docker build -t sqlatenwiki/peppermintywiki:master -t sqlatenwiki/peppermintywiki:dev /home/sql/myimages
-echo "###PUSHING master###"
-docker push sqlatenwiki/peppermintywiki:master
-docker push sqlatenwiki/peppermintywiki:dev
+docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -t sqlatenwiki/peppermintywiki:dev -t sqlatenwiki/peppermintywiki:master . --push
