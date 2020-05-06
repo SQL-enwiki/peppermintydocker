bde37d552b382279e5d26b30d601957c5c1569fc
SQL At ENWiki <45909509+SQL-enwiki@users.noreply.github.com>
2020-05-06 18:45:34 -0400

    Add latest tag

--- update.sh
+++ update.sh
@@ -2,27 +2,30 @@
 
 cd /home/sql/myimages
 rm -rf /home/sql/myimages/webroot
-echo "###CLONING LATEST RELEASE###"
+echo "###CLONING###"
 git clone https://github.com/sbrl/Pepperminty-Wiki.git webroot
 cd /home/sql/myimages/webroot
 LATEST="$(git describe --abbrev=0 --tags)"
-echo "###CHECKING OUT RELEASE $LATEST###"
+echo "###CHECKING OUT $LATEST###"
 git checkout $LATEST
 cd /home/sql/myimages
-echo "###BUILDING RELEASE $LATEST###"
-docker build -t sqlatenwiki/peppermintywiki:$LATEST /home/sql/myimages
-echo "###PUSHING RELEASE $LATEST###"
+echo "###BUILDING $LATEST###"
+docker build -t sqlatenwiki/peppermintywiki:$LATEST -t sqlatenwiki/peppermintywiki:stable /home/sql/myimages
+echo "###PUSHING $LATEST###"
 docker push sqlatenwiki/peppermintywiki:$LATEST
+docker push sqlatenwiki/peppermintywiki:stable
+docker push sqlatenwiki/peppermintywiki:latest
 
 
-cd /home/sql/myimages                                                                                                                                                                             
-rm -rf /home/sql/myimages/webroot                                                                                                                                                                 
-echo "###CLONING MASTER###"                                                                                                                                                                              
-git clone https://github.com/sbrl/Pepperminty-Wiki.git webroot                                                                                                                                    
-cd /home/sql/myimages/webroot                                                                                                                                                                     
-echo "###CHECKING OUT MASTER###"
+cd /home/sql/myimages
+rm -rf /home/sql/myimages/webroot
+echo "###CLONING###"
+git clone https://github.com/sbrl/Pepperminty-Wiki.git webroot
+cd /home/sql/myimages/webroot
+chown www-data:www-data * -R
 cd /home/sql/myimages
 echo "###BUILDING MASTER###"
-docker build -t sqlatenwiki/peppermintywiki:master /home/sql/myimages
+docker build -t sqlatenwiki/peppermintywiki:master -t sqlatenwiki/peppermintywiki:dev /home/sql/myimages
 echo "###PUSHING master###"
 docker push sqlatenwiki/peppermintywiki:master
+docker push sqlatenwiki/peppermintywiki:dev
